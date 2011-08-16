c82cdbac6516d09cd00862d74ad5864cd7d8257f
Richard Dingwall <rdingwall@gmail.com>
2011-08-16 22:09:16 +0100

    Added benchmarks

---
+++ src/ProtoBuf.Data.Benchmark/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ProtoBuf.Data.Benchmark
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                // This benchmark requires the AdventureWorks DW database. You
+                // can grab it from here: http://msftdbprodsamples.codeplex.com/
+                var connectionString = new SqlConnectionStringBuilder
+                                           {
+                                               DataSource = @".\SQLEXPRESS",
+                                               InitialCatalog = "AdventureWorksDW2008R2",
+                                               IntegratedSecurity = true
+                                           };
+
+                new Benchmark(connectionString.ConnectionString).Run();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                throw;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
