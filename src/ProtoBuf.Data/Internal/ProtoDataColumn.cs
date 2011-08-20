596290363d2939c632d9b56fc350cb40408bfa45
Richard Dingwall <rdingwall@gmail.com>
2011-08-20 10:56:24 +0100

    Deleted some unused properties from old DTO serialization.

--- src/ProtoBuf.Data/Internal/ProtoDataColumn.cs
+++ src/ProtoBuf.Data/Internal/ProtoDataColumn.cs
@@ -12,25 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System;
+
 namespace ProtoBuf.Data.Internal
 {
-    [ProtoContract]
     public class ProtoDataColumn
     {
-        [ProtoMember(1)]
-        public int Ordinal;
-
-        [ProtoMember(2)]
-        public bool IsNullable;
-
-        [ProtoMember(3, IsRequired =true)]
         public ProtoDataType ProtoDataType;
-
-        [ProtoMember(4)]
         public string ColumnName;
-
-        [ProtoMember(5)]
-        public int OrdinalWithinType;
+        public int Ordinal;
 
         public override string ToString()
         {
