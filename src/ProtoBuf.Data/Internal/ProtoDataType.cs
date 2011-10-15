e17f8fd53c64156a61c247e0d4f7ce38cc6315fb
Richard Dingwall <rdingwall@gmail.com>
2011-10-15 12:25:41 +0100

    Added missing license header to a few files.

--- src/ProtoBuf.Data/Internal/ProtoDataType.cs
+++ src/ProtoBuf.Data/Internal/ProtoDataType.cs
@@ -1,5 +1,4 @@
-﻿
-// Copyright 2011 Richard Dingwall - http://richarddingwall.name
+﻿// Copyright 2011 Richard Dingwall - http://richarddingwall.name
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
