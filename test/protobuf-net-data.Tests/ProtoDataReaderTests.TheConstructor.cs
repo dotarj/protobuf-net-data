89ab49c1aee04a4a699f3cbe2874ae11a6e2d2ca
Arjen Post <mail@arjenpost.nl>
2018-03-25 08:21:36 +0200

    ProtoDataReader ctor test

---
+++ test/protobuf-net-data.Tests/ProtoDataReaderTests.TheConstructor.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Richard Dingwall, Arjen Post. See LICENSE in the project root for license information.
+
+using System;
+using System.Data;
+using System.IO;
+using Xunit;
+
+namespace ProtoBuf.Data.Tests
+{
+    public partial class ProtoDataReaderTests
+    {
+        public class TheConstructor : ProtoDataReaderTests
+        {
+            [Fact]
+            public void ShouldThrowIfStreamIsNull()
+            {
+                // Assert
+                Assert.Throws<ArgumentNullException>(() => new ProtoDataReader(null));
+            }
+        }
+    }
+}
