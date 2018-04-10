4e37eb7d4c6a5da0a9be20defd75982a04c62427
Arjen Post <mail@arjenpost.nl>
2018-04-10 20:26:22 +0200

    benchmark project added

---
+++ benchmark/protobuf-net-data.Benchmarks/Program.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Richard Dingwall, Arjen Post. See LICENSE in the project root for license information.
+
+using System;
+using BenchmarkDotNet.Running;
+
+namespace ProtoBuf.Data.Benchmarks
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<SerializeBenchmark>();
+            BenchmarkRunner.Run<DeserializeBenchmark>();
+
+            Console.Read();
+        }
+    }
+}
---
+++ benchmark/protobuf-net-data.Benchmarks/SerializeBenchmark.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Richard Dingwall, Arjen Post. See LICENSE in the project root for license information.
+
+using System;
+using System.Data;
+using System.IO;
+using BenchmarkDotNet.Attributes;
+
+namespace ProtoBuf.Data.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class SerializeBenchmark
+    {
+        private static readonly DataTable SchemaTable;
+
+        static SerializeBenchmark()
+        {
+            SchemaTable = new DataTable();
+
+            SchemaTable.Columns.Add("ColumnName", typeof(string));
+            SchemaTable.Columns.Add("DataType", typeof(Type));
+
+            SchemaTable.Rows.Add("foo", typeof(int));
+            SchemaTable.Rows.Add("bar", typeof(Guid));
+            SchemaTable.Rows.Add("baz", typeof(float));
+        }
+
+        [Benchmark]
+        public void Run()
+        {
+            var dataReader = new DataReaderMock(SchemaTable);
+
+            using (var memoryStream = new MemoryStream())
+            {
+                DataSerializer.Serialize(memoryStream, dataReader);
+            }
+        }
+    }
+}
