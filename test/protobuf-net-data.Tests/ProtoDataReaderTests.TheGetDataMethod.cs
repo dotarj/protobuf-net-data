8f9460f7d677dc22ed068dc27666e8c8433cc0a2
Arjen Post <mail@arjenpost.nl>
2018-04-11 20:09:21 +0200

    ProtoDataReader unit tests

---
+++ test/protobuf-net-data.Tests/ProtoDataReaderTests.TheGetDataMethod.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Richard Dingwall, Arjen Post. See LICENSE in the project root for license information.
+
+using System;
+using System.Data;
+using Xunit;
+
+namespace ProtoBuf.Data.Tests
+{
+    public partial class ProtoDataReaderTests
+    {
+        public class TheGetDataMethod : ProtoDataReaderTests
+        {
+            [Fact]
+            public void ShouldThrowException()
+            {
+                // Arrange
+                var dataReader = DataReaderHelper.CreateDataReader(value: true);
+
+                // Assert
+                Assert.Throws<NotSupportedException>(() => ((IDataReader)dataReader).GetData(0));
+            }
+        }
+    }
+}
