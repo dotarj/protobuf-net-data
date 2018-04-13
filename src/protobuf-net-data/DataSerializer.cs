f1bd07cfe564c83b55f741c3523197af7fef5dd7
Arjen Post <mail@arjenpost.nl>
2018-04-13 18:08:23 +0200

    DataSerializer documentation

--- src/protobuf-net-data/DataSerializer.cs
+++ src/protobuf-net-data/DataSerializer.cs
@@ -7,27 +7,27 @@ using System.IO;
 namespace ProtoBuf.Data
 {
     /// <summary>
-    /// Provides protocol-buffer serialization for <see cref="System.Data.IDataReader"/>s.
+    /// Provides protocol-buffer serialization for <see cref="IDataReader"/>s.
     /// </summary>
     public static class DataSerializer
     {
         private static readonly IDataSerializerEngine Engine = new DataSerializerEngine();
 
         /// <summary>
-        /// Serialize an <see cref="System.Data.IDataReader"/> to a binary stream using protocol-buffers.
+        /// Serialize an <see cref="IDataReader"/> to a binary stream using protocol-buffers.
         /// </summary>
-        /// <param name="stream">The <see cref="System.IO.Stream"/> to write to.</param>
-        /// <param name="reader">The <see cref="System.Data.IDataReader"/> who's contents to serialize.</param>
+        /// <param name="stream">The <see cref="Stream"/> to write to.</param>
+        /// <param name="reader">The <see cref="IDataReader"/> who's contents to serialize.</param>
         public static void Serialize(Stream stream, IDataReader reader)
         {
             Engine.Serialize(stream, reader);
         }
 
         /// <summary>
-        /// Serialize an <see cref="System.Data.IDataReader"/> to a binary stream using protocol-buffers.
+        /// Serialize an <see cref="IDataReader"/> to a binary stream using protocol-buffers.
         /// </summary>
-        /// <param name="stream">The <see cref="System.IO.Stream"/> to write to.</param>
-        /// <param name="reader">The <see cref="System.Data.IDataReader"/> who's contents to serialize.</param>
+        /// <param name="stream">The <see cref="Stream"/> to write to.</param>
+        /// <param name="reader">The <see cref="IDataReader"/> who's contents to serialize.</param>
         /// <param name="options"><see cref="ProtoDataWriterOptions"/> specifying any custom serialization options.</param>
         public static void Serialize(Stream stream, IDataReader reader, ProtoDataWriterOptions options)
         {
@@ -35,20 +35,20 @@ namespace ProtoBuf.Data
         }
 
         /// <summary>
-        /// Serialize a <see cref="System.Data.DataTable"/> to a binary stream using protocol-buffers.
+        /// Serialize a <see cref="DataTable"/> to a binary stream using protocol-buffers.
         /// </summary>
-        /// <param name="stream">The <see cref="System.IO.Stream"/> to write to.</param>
-        /// <param name="dataTable">The <see cref="System.Data.DataTable"/> who's contents to serialize.</param>
+        /// <param name="stream">The <see cref="Stream"/> to write to.</param>
+        /// <param name="dataTable">The <see cref="DataTable"/> who's contents to serialize.</param>
         public static void Serialize(Stream stream, DataTable dataTable)
         {
             Engine.Serialize(stream, dataTable);
         }
 
         /// <summary>
-        /// Serialize a <see cref="System.Data.DataTable"/> to a binary stream using protocol-buffers.
+        /// Serialize a <see cref="DataTable"/> to a binary stream using protocol-buffers.
         /// </summary>
-        /// <param name="stream">The <see cref="System.IO.Stream"/> to write to.</param>
-        /// <param name="dataTable">The <see cref="System.Data.DataTable"/> who's contents to serialize.</param>
+        /// <param name="stream">The <see cref="Stream"/> to write to.</param>
+        /// <param name="dataTable">The <see cref="DataTable"/> who's contents to serialize.</param>
         /// <param name="options"><see cref="ProtoDataWriterOptions"/> specifying any custom serialization options.</param>
         public static void Serialize(Stream stream, DataTable dataTable, ProtoDataWriterOptions options)
         {
@@ -56,20 +56,20 @@ namespace ProtoBuf.Data
         }
 
         /// <summary>
-        /// Serialize a <see cref="System.Data.DataSet"/> to a binary stream using protocol-buffers.
+        /// Serialize a <see cref="DataSet"/> to a binary stream using protocol-buffers.
         /// </summary>
-        /// <param name="stream">The <see cref="System.IO.Stream"/> to write to.</param>
-        /// <param name="dataSet">The <see cref="System.Data.DataSet"/> who's contents to serialize.</param>
+        /// <param name="stream">The <see cref="Stream"/> to write to.</param>
+        /// <param name="dataSet">The <see cref="DataSet"/> who's contents to serialize.</param>
         public static void Serialize(Stream stream, DataSet dataSet)
         {
             Engine.Serialize(stream, dataSet);
         }
 
         /// <summary>
-        /// Serialize a <see cref="System.Data.DataSet"/> to a binary stream using protocol-buffers.
+        /// Serialize a <see cref="DataSet"/> to a binary stream using protocol-buffers.
         /// </summary>
-        /// <param name="stream">The <see cref="System.IO.Stream"/> to write to.</param>
-        /// <param name="dataSet">The <see cref="System.Data.DataSet"/> who's contents to serialize.</param>
+        /// <param name="stream">The <see cref="Stream"/> to write to.</param>
+        /// <param name="dataSet">The <see cref="DataSet"/> who's contents to serialize.</param>
         /// <param name="options"><see cref="ProtoDataWriterOptions"/> specifying any custom serialization options.</param>
         public static void Serialize(Stream stream, DataSet dataSet, ProtoDataWriterOptions options)
         {
@@ -77,38 +77,42 @@ namespace ProtoBuf.Data
         }
 
         /// <summary>
-        /// Deserialize a protocol-buffer binary stream back into an <see cref="System.Data.IDataReader"/>.
+        /// Deserialize a protocol-buffer binary stream back into an <see cref="IDataReader"/>.
         /// </summary>
-        /// <param name="stream">The <see cref="System.IO.Stream"/> to read from.</param>
+        /// <param name="stream">The <see cref="Stream"/> to read from.</param>
+        /// <returns>The <see cref="IDataReader"/> being deserialized.</returns>
         public static IDataReader Deserialize(Stream stream)
         {
             return Engine.Deserialize(stream);
         }
 
         /// <summary>
-        /// Deserialize a protocol-buffer binary stream back into a <see cref="System.Data.DataTable"/>.
+        /// Deserialize a protocol-buffer binary stream back into a <see cref="DataTable"/>.
         /// </summary>
-        /// <param name="stream">The <see cref="System.IO.Stream"/> to read from.</param>
+        /// <param name="stream">The <see cref="Stream"/> to read from.</param>
+        /// <returns>The <see cref="DataTable"/> being deserialized.</returns>
         public static DataTable DeserializeDataTable(Stream stream)
         {
             return Engine.DeserializeDataTable(stream);
         }
 
         /// <summary>
-        /// Deserialize a protocol-buffer binary stream back into a <see cref="System.Data.DataSet"/>.
+        /// Deserialize a protocol-buffer binary stream back into a <see cref="DataSet"/>.
         /// </summary>
-        /// <param name="stream">The <see cref="System.IO.Stream"/> to read from.</param>
-        /// <param name="tables">A sequence of strings, from which the <see cref="System.Data.DataSet"/> Load method retrieves table name information.</param>
+        /// <param name="stream">The <see cref="Stream"/> to read from.</param>
+        /// <param name="tables">A sequence of strings, from which the <see cref="DataSet"/> Load method retrieves table name information.</param>
+        /// <returns>The <see cref="DataSet"/> being deserialized.</returns>
         public static DataSet DeserializeDataSet(Stream stream, IEnumerable<string> tables)
         {
             return Engine.DeserializeDataSet(stream, tables);
         }
 
         /// <summary>
-        /// Deserialize a protocol-buffer binary stream as a <see cref="System.Data.DataSet"/>.
+        /// Deserialize a protocol-buffer binary stream as a <see cref="DataSet"/>.
         /// </summary>
-        /// <param name="stream">The <see cref="System.IO.Stream"/> to read from.</param>
-        /// <param name="tables">An array of strings, from which the <see cref="System.Data.DataSet"/> Load method retrieves table name information.</param>
+        /// <param name="stream">The <see cref="Stream"/> to read from.</param>
+        /// <param name="tables">An array of strings, from which the <see cref="DataSet"/> Load method retrieves table name information.</param>
+        /// <returns>The <see cref="DataSet"/> being deserialized.</returns>
         public static DataSet DeserializeDataSet(Stream stream, params string[] tables)
         {
             return Engine.DeserializeDataSet(stream, tables);
--- src/protobuf-net-data/IDataSerializerEngine.cs
+++ src/protobuf-net-data/IDataSerializerEngine.cs
@@ -7,79 +7,83 @@ using System.IO;
 namespace ProtoBuf.Data
 {
     /// <summary>
-    /// Provides protocol-buffer serialization for <see cref="System.Data.IDataReader"/>s.
+    /// Provides protocol-buffer serialization for <see cref="IDataReader"/>s.
     /// </summary>
     public interface IDataSerializerEngine
     {
         /// <summary>
-        /// Serialize an <see cref="System.Data.IDataReader"/> to a binary stream using protocol-buffers.
+        /// Serialize an <see cref="IDataReader"/> to a binary stream using protocol-buffers.
         /// </summary>
-        /// <param name="stream">The <see cref="System.IO.Stream"/> to write to.</param>
-        /// <param name="reader">The <see cref="System.Data.IDataReader"/> who's contents to serialize.</param>
+        /// <param name="stream">The <see cref="Stream"/> to write to.</param>
+        /// <param name="reader">The <see cref="IDataReader"/> who's contents to serialize.</param>
         void Serialize(Stream stream, IDataReader reader);
 
         /// <summary>
-        /// Serialize a <see cref="System.Data.DataTable"/> to a binary stream using protocol-buffers.
+        /// Serialize a <see cref="DataTable"/> to a binary stream using protocol-buffers.
         /// </summary>
-        /// <param name="stream">The <see cref="System.IO.Stream"/> to write to.</param>
-        /// <param name="dataTable">The <see cref="System.Data.DataTable"/> who's contents to serialize.</param>
+        /// <param name="stream">The <see cref="Stream"/> to write to.</param>
+        /// <param name="dataTable">The <see cref="DataTable"/> who's contents to serialize.</param>
         void Serialize(Stream stream, DataTable dataTable);
 
         /// <summary>
-        /// Serialize a <see cref="System.Data.DataSet"/> to a binary stream using protocol-buffers.
+        /// Serialize a <see cref="DataSet"/> to a binary stream using protocol-buffers.
         /// </summary>
-        /// <param name="stream">The <see cref="System.IO.Stream"/> to write to.</param>
-        /// <param name="dataSet">The <see cref="System.Data.DataSet"/> who's contents to serialize.</param>
+        /// <param name="stream">The <see cref="Stream"/> to write to.</param>
+        /// <param name="dataSet">The <see cref="DataSet"/> who's contents to serialize.</param>
         void Serialize(Stream stream, DataSet dataSet);
 
         /// <summary>
-        /// Serialize an <see cref="System.Data.IDataReader"/> to a binary stream using protocol-buffers.
+        /// Serialize an <see cref="IDataReader"/> to a binary stream using protocol-buffers.
         /// </summary>
-        /// <param name="stream">The <see cref="System.IO.Stream"/> to write to.</param>
-        /// <param name="reader">The <see cref="System.Data.IDataReader"/> who's contents to serialize.</param>
+        /// <param name="stream">The <see cref="Stream"/> to write to.</param>
+        /// <param name="reader">The <see cref="IDataReader"/> who's contents to serialize.</param>
         /// <param name="options"><see cref="ProtoDataWriterOptions"/> specifying any custom serialization options.</param>
         void Serialize(Stream stream, IDataReader reader, ProtoDataWriterOptions options);
 
         /// <summary>
-        /// Serialize a <see cref="System.Data.DataTable"/> to a binary stream using protocol-buffers.
+        /// Serialize a <see cref="DataTable"/> to a binary stream using protocol-buffers.
         /// </summary>
-        /// <param name="stream">The <see cref="System.IO.Stream"/> to write to.</param>
-        /// <param name="dataTable">The <see cref="System.Data.DataTable"/> who's contents to serialize.</param>
+        /// <param name="stream">The <see cref="Stream"/> to write to.</param>
+        /// <param name="dataTable">The <see cref="DataTable"/> who's contents to serialize.</param>
         /// <param name="options"><see cref="ProtoDataWriterOptions"/> specifying any custom serialization options.</param>
         void Serialize(Stream stream, DataTable dataTable, ProtoDataWriterOptions options);
 
         /// <summary>
-        /// Serialize a <see cref="System.Data.DataSet"/> to a binary stream using protocol-buffers.
+        /// Serialize a <see cref="DataSet"/> to a binary stream using protocol-buffers.
         /// </summary>
-        /// <param name="stream">The <see cref="System.IO.Stream"/> to write to.</param>
-        /// <param name="dataSet">The <see cref="System.Data.DataSet"/> who's contents to serialize.</param>
+        /// <param name="stream">The <see cref="Stream"/> to write to.</param>
+        /// <param name="dataSet">The <see cref="DataSet"/> who's contents to serialize.</param>
         /// <param name="options"><see cref="ProtoDataWriterOptions"/> specifying any custom serialization options.</param>
         void Serialize(Stream stream, DataSet dataSet, ProtoDataWriterOptions options);
 
         /// <summary>
-        /// Deserialize a protocol-buffer binary stream back into an <see cref="System.Data.IDataReader"/>.
+        /// Deserialize a protocol-buffer binary stream back into an <see cref="IDataReader"/>.
         /// </summary>
-        /// <param name="stream">The <see cref="System.IO.Stream"/> to read from.</param>
+        /// <param name="stream">The <see cref="Stream"/> to read from.</param>
+        /// <returns>The <see cref="IDataReader"/> being deserialized.</returns>
         IDataReader Deserialize(Stream stream);
 
         /// <summary>
-        /// Deserialize a protocol-buffer binary stream back into a <see cref="System.Data.DataTable"/>.
+        /// Deserialize a protocol-buffer binary stream back into a <see cref="DataTable"/>.
         /// </summary>
-        /// <param name="stream">The <see cref="System.IO.Stream"/> to read from.</param>
+        /// <param name="stream">The <see cref="Stream"/> to read from.</param>
+        /// <returns>The <see cref="DataTable"/> being deserialized.</returns>
         DataTable DeserializeDataTable(Stream stream);
 
         /// <summary>
-        /// Deserialize a protocol-buffer binary stream back into a <see cref="System.Data.DataSet"/>.
+        /// Deserialize a protocol-buffer binary stream back into a <see cref="DataSet"/>.
         /// </summary>
-        /// <param name="stream">The <see cref="System.IO.Stream"/> to read from.</param>
-        /// <param name="tables">A sequence of strings, from which the <see cref="System.Data.DataSet"/> Load method retrieves table name information.</param>
+        /// <param name="stream">The <see cref="Stream"/> to read from.</param>
+        /// <param name="tables">A sequence of strings, from which the <see cref="DataSet"/> Load method retrieves table name information.</param>
+        /// <returns>The <see cref="DataSet"/> being deserialized.</returns>
         DataSet DeserializeDataSet(Stream stream, IEnumerable<string> tables);
 
         /// <summary>
-        /// Deserialize a protocol-buffer binary stream as a <see cref="System.Data.DataSet"/>.
+        /// Deserialize a protocol-buffer binary stream as a <see cref="DataSet"/>.
         /// </summary>
-        /// <param name="stream">The <see cref="System.IO.Stream"/> to read from.</param>
-        /// <param name="tables">An array of strings, from which the <see cref="System.Data.DataSet"/> Load method retrieves table name information.</param>
+        /// <param name="stream">The <see cref="Stream"/> to read from.</param>
+        /// <param name="tables">An array of strings, from which the <see cref="DataSet"/> Load method retrieves table name information.</param>
+        /// <returns>The <see cref="DataSet"/> being deserialized.</returns>
         DataSet DeserializeDataSet(Stream stream, params string[] tables);
     }
 }
