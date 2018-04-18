1605cbc838deb562a423eef510ff343fd1b46984
Arjen Post <mail@arjenpost.nl>
2018-04-18 07:31:50 +0200

    ProtoDataStream unit tests

---
+++ test/protobuf-net-data.Tests/ProtoDataStreamTests.TheCanWriteProperty.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Richard Dingwall, Arjen Post. See LICENSE in the project root for license information.
+
+using Xunit;
+
+namespace ProtoBuf.Data.Tests
+{
+    public partial class ProtoDataStreamTests
+    {
+        public class TheCanWriteProperty : ProtoDataStreamTests
+        {
+            [Fact]
+            public void ShouldReturnFalse()
+            {
+                // Arrange
+                var stream = new ProtoDataStream(DataReaderHelper.CreateDataReader("foo"));
+
+                // Act
+                var result = stream.CanWrite;
+
+                // Assert
+                Assert.False(result);
+            }
+        }
+    }
+}
---
+++ test/protobuf-net-data.Tests/ProtoDataStreamTests.TheLengthProperty.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Richard Dingwall, Arjen Post. See LICENSE in the project root for license information.
+
+using System;
+using Xunit;
+
+namespace ProtoBuf.Data.Tests
+{
+    public partial class ProtoDataStreamTests
+    {
+        public class TheLengthProperty : ProtoDataStreamTests
+        {
+            [Fact]
+            public void ShouldThrowException()
+            {
+                // Arrange
+                var stream = new ProtoDataStream(DataReaderHelper.CreateDataReader("foo"));
+
+                // Assert
+                Assert.Throws<NotSupportedException>(() => stream.Length);
+            }
+        }
+    }
+}
