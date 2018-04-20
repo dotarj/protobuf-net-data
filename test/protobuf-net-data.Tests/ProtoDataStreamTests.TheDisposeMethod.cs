1f005a8bef417dc4aa9ddd1241adf1a9b466fd61
Arjen Post <mail@arjenpost.nl>
2018-04-20 07:32:40 +0200

    ProtoDataStream.Dispose unit tests

---
+++ test/protobuf-net-data.Tests/ProtoDataStreamTests.TheDisposeMethod.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Richard Dingwall, Arjen Post. See LICENSE in the project root for license information.
+
+using System;
+using Xunit;
+
+namespace ProtoBuf.Data.Tests
+{
+    public partial class ProtoDataStreamTests
+    {
+        public class TheDisposeMethod : ProtoDataStreamTests
+        {
+            [Fact]
+            public void ShouldDisposeReader()
+            {
+                // Arrange
+                var reader = this.CreateDataReader("foo");
+                var stream = new ProtoDataStream(reader);
+
+                // Act
+                reader.Dispose();
+
+                // Assert
+                Assert.Throws<InvalidOperationException>(() => reader.Read());
+            }
+        }
+    }
+}
